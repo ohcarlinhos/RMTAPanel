4e10003bc4120b1a701289bd420f0bf414cb8445
Carlos Roberto <crpu4a@gmail.com>
2024-08-09 19:14:45 -0300

    feat: add AuthenticateService

--- RMTAPanel/Program.cs
+++ RMTAPanel/Program.cs
@@ -5,6 +5,7 @@ using MudBlazor.Services;
 using Blazored.LocalStorage;
 using RMTAPanel;
 using RMTAPanel.Providers;
+using RMTAPanel.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -23,6 +24,8 @@ builder.Services.AddScoped<CustomAuthStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>((provider) =>
     provider.GetRequiredService<CustomAuthStateProvider>()
 );
+
+builder.Services.AddScoped<AuthenticateService>();
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
---
+++ RMTAPanel/Services/AuthenticateService.cs
@@ -0,0 +1,20 @@
+using Blazored.LocalStorage;
+using RMTAPanel.Providers;
+
+namespace RMTAPanel.Services;
+
+public class AuthenticateService(ILocalStorageService LocalStorage, CustomAuthStateProvider AuthProvider)
+{
+    public async Task SetSate(string? token)
+    {
+        if (string.IsNullOrEmpty(token)) throw new Exception("empty_token");
+        await LocalStorage!.SetItemAsync("jwt_token", token);
+        await AuthProvider!.NotifyStateChanged();
+    }
+
+    public async Task ClearSate()
+    {
+        await LocalStorage!.RemoveItemAsync("jwt_token");
+        await AuthProvider!.NotifyStateChanged();
+    }
+}

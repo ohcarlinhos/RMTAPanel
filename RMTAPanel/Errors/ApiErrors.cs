fde3cf5c686eaaade42620a1abff7d869d311c63
Carlos Júnior <crpu4a@gmail.com>
2024-08-14 13:20:19 -0300

    feat: add api error const

--- RMTAPanel/Errors/ApiErrors.cs
+++ RMTAPanel/Errors/ApiErrors.cs
@@ -3,4 +3,5 @@ namespace RMTAPanel.Errors;
 public static class ApiErrors
 {
     public const string GenericErrorServe = "Não foi possível executar essa ação.";
+    public const string Forbidden = "Você não possui permissão para acessar esse recurso.";
 }
